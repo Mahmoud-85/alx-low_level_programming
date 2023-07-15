cf393b127eb990ff9ffd9353ed63d1755fd7e7c5
Mahmoud-85 <ma7moud0291@gmail.com>
2023-07-14 19:05:02 -0700

    e

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,8 @@
 /**
  * main - entry point
  *
- * Description: print all single digit numbers
+ * Description: print 0-9 using putchar
+ *              while using int variable
  *
  * Return: Always 0 (success)
 */
@@ -14,10 +15,11 @@ int main(void)
 
 	while (digit <= 9)
 	{
-		printf("%i", digit);
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
 		digit++;
 	}
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
